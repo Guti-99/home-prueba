7ab4779d265a3bfed05d6331204203cf93855a76
Guti-99 <nahuel.gt20@gmail.com>
2020-09-25 19:21:52 -0300

    carrusel

--- cabeceraFea/scripts.js
+++ cabeceraFea/scripts.js
@@ -13,3 +13,16 @@ jQuery('document').ready(function($){
 
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel')
+    M.Carousel.init(elementosCarousel, {
+        duration: 1500,
+        dist: -100,
+        shift: 5,
+        padding: 5,
+        numVisible: 4,
+        indicators: true,
+        noWrap: false,
+    });
+});
